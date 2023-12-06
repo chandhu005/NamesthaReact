0b1d90969ec87355dbd4e48d8fafba6feb9dff50
ChandraSekhar <115386409+chandhu005@users.noreply.github.com>
2023-12-06 07:25:29 -0800

    Add files via upload

---
+++ Day1-inception/IntrotoReact/App.js
@@ -0,0 +1,7 @@
+var heading=React.createElement("h2",{id:"heading"},"Chandhu😘Chitti")
+      
+
+const parent=React.createElement("div",{id:"parent"},
+React.createElement("div",{id:"child"},[React.createElement("h1",{id:"child"},"Chitti😍😍😍"),React.createElement("h1",{id:"child"},"Chandhu😘😘😘😘😘")]));
+var root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent)
